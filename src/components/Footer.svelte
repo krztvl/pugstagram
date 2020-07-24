cda384226f80bb2c5f434c68dbb419699f5bb2f1
Krztvl <c.penatroncoso@gmail.com>
2020-07-23 21:03:15 -0400

    Clase 23: Header likes

---
+++ src/components/Footer.svelte
@@ -0,0 +1,18 @@
+<script>
+  let currentYear = new Date().getFullYear();
+</script>
+
+<style>
+  .Footer {
+    font-size: 11px;
+    letter-spacing: 1px;
+    font-weight: normal;
+    color: #c7c7c7;
+  }
+</style>
+
+<div class="Footer">
+  <div class="Footer-container">
+    <div class="Footer-copy">© {currentYear} PUGSTAGRAM FROM PLATZI</div>
+  </div>
+</div>
---
+++ src/components/TimeLine.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Card from "./Card.svelte";
+  import LoadingSpinner from "./LoadingSpinner.svelte";
+
+  export let posts = [];
+</script>
+
+<style>
+  .TimeLine {
+    padding: 4em 0 0 0;
+  }
+</style>
+
+<div class="TimeLine">
+  <div class="TimeLine-container">
+    {#each posts as post}
+      <Card {...post} />
+    {:else}
+      <LoadingSpinner />
+    {/each}
+  </div>
+
+</div>
